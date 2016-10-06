3d9e17ab8abcf997b10c720affd87e531c86ab82
Tom Boutell <tom@punkave.com>
2016-10-06 11:46:42 -0400

    don't try to monkeypatch 0.10 itself

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var superBufferWrite = Buffer.prototype.write;
+var version = process.version;
 
-// Error: Buffer.write(string, encoding, offset[, length]) is no longer supported
+// Don't try to patch 0.10 to call newer APIs that are not there
+if (!version.match(/^0\.10/)) {
 
-Buffer.prototype.write = function(one, two, three, four) {
-  if ((arguments.length === 3) && (typeof(arguments[1]) === 'string')) {
-    return superBufferWrite.call(this, one, three, this.length - three, two);
-  } else if ((arguments.length === 4) && (typeof(arguments[1]) === 'string')) {
-    return superBufferWrite.call(this, one, three, four, two);
-  } else {
-    return superBufferWrite.apply(this, arguments);
-  }
-};
+  // Error: Buffer.write(string, encoding, offset[, length]) is no longer supported
+  var superBufferWrite = Buffer.prototype.write;
+  Buffer.prototype.write = function(one, two, three, four) {
+    if ((arguments.length === 3) && (typeof(arguments[1]) === 'string')) {
+      return superBufferWrite.call(this, one, three, this.length - three, two);
+    } else if ((arguments.length === 4) && (typeof(arguments[1]) === 'string')) {
+      return superBufferWrite.call(this, one, three, four, two);
+    } else {
+      return superBufferWrite.apply(this, arguments);
+    }
+  };
+
+}
